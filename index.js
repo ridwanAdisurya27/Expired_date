3ae52cb26d3e53f6955c670d5266811052a84c6c
ridwanAdisurya27 <boymargono70@gmail.com>
2025-04-25 14:30:19 +0700

    memperbaiki env file

--- index.js
+++ index.js
@@ -3,19 +3,29 @@ const express = require('express');
 const axios = require('axios');
 const {google} = require("googleapis");
 const bodyParser = require("body-parser");
-const credentials = JSON.parse(process.env.GOOGLE_CREDENTIALS);
 const app = express();
 const PORT = process.env.PORT || 3000
 const spreadsheetId = "1FoFKjs65XJnmatbUqRPm-PMvmJJqVkkXOa9do6f_yeo";
-
+const credentials = {
+    type: process.env.GOOGLE_TYPE,
+    project_id: process.env.GOOGLE_PROJECT_ID,
+    private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    client_id: process.env.GOOGLE_CLIENT_ID,
+    auth_uri: process.env.GOOGLE_AUTH_URI,
+    token_uri: process.env.GOOGLE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_CERT_URL,
+    client_x509_cert_url: process.env.GOOGLE_CLIENT_CERT_URL,
+};
 app.use(express.json());
 app.use(bodyParser.json());
 
 // Auth Google
 const auth = new google.auth.GoogleAuth({
-    credentials : credentials,
+    credentials: credentials,
     scopes: ["https://www.googleapis.com/auth/spreadsheets"],
-});
+    });
 
 // ROUTE TEST
 app.get('/', (req, res) => {
